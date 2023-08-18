2d17741cfed3b62f24ca29a8c0b855268014dcd0
Noah Razzaq <nrazzaq@vinatis.com>
2023-08-18 15:46:39 +0200

    :lipstick: adding carousel of products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,51 @@
 
     <div>
         <h1>Les produits disponible aujourd'hui </h1>
-        @foreach ($products as $product)
-            <div class="w-full md:w-1/3 p-10">
-                <x-product-card :product="$product" />
-            </div>
-        @endforeach
     </div>
+
+    <div class="w-full overflow-hidden">
+        <div class="flex" id="carousel">
+            @foreach ($products as $product)
+                <div class="w-1/4 px-9">
+                    <x-product-card :product="$product" />
+                </div>
+            @endforeach
+        </div>
+    </div>
+    
+    <div class="flex justify-between mt-4">
+        <button id="prevBtn" class="px-2 py-1 bg-gray-300">Précédent</button>
+        <button id="nextBtn" class="px-2 py-1 bg-gray-300">Suivant</button>
+    </div>
+    
+
+
 
 
 </x-app-layout>
+
+<script>
+    const carousel = document.getElementById('carousel');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    const cardWidth = carousel.firstElementChild.clientWidth;
+    const visibleCards = 4; // Nombre de cartes visibles à la fois
+    const step = cardWidth * visibleCards;
+
+    let translateX = 0;
+
+    prevBtn.addEventListener('click', () => {
+        if (translateX < 0) {
+            translateX += step;
+            carousel.style.transform = `translateX(${translateX}px)`;
+        }
+    });
+
+    nextBtn.addEventListener('click', () => {
+        const maxTranslateX = -(carousel.scrollWidth - carousel.clientWidth);
+        if (translateX > maxTranslateX) {
+            translateX -= step;
+            carousel.style.transform = `translateX(${translateX}px)`;
+        }
+    });
+</script>
